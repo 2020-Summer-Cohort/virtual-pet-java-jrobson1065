e8e69ae9cd80b374655c07e8939c1c5c7521c64a
JRobson1065 <j.robson1065@gmail.com>
2020-05-15 13:27:18 -0400

    firs

---
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -0,0 +1,31 @@
+package virtual_pet;
+import java.util.Random;
+
+public class VirtualPet {
+
+    public void myPet(String[] args) {
+        Random rand = new Random();
+        int hunger = rand.nextInt(10) + 1;
+        int thirst;
+        int health;
+        System.out.println(hunger);
+    }
+
+    public void feedPet() {
+
+    }
+
+    public void waterPet() {
+
+    }
+
+    public void talkToPet() {
+
+    }
+
+    public void giveLight() {
+
+    }
+
+
+}
